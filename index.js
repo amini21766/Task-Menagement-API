d7c0dcc5e36d3d4a493425c7663749ff2e03cf85
amini21766 <amini21766@gmail.com>
2024-10-10 11:25:56 +0430

    The tasks route created

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
 
 // routes
 app.get("/hello", (req, res) => {
   res.send("Task Management API.");
 });
 
+app.use("/api/v1/tasks", tasks);
 
-
-
+// listener
 const port = 4000;
 app.listen(port, console.log(`The server is running on port ${port}...`));
