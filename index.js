c5b42a4ca448ab0567a178d2e7229259c32b1fe3
amini21766 <amini21766@gmail.com>
2024-10-10 10:54:39 +0430

    The listener and first get request created

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log('Welcome To API project!')
-console.log('This is Api project!');
+const express = require("express");
+const app = express();
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Task Management API.");
+});
+
+
+
+
+const port = 4000;
+app.listen(port, console.log(`The server is running on port ${port}...`));
